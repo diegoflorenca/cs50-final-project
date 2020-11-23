6b18c7fd87e3b09e3e6682fcfef038eacc2ac1d9
Diego Felipe Florença <diegoflorenca@gmail.com>
2020-11-23 20:20:50 -0300

    fixed untracked files

--- db/SQL/CREATE_ITEMS.sql
+++
@@ -1,2 +0,0 @@
--- SQLite
-CREATE TABLE 'items' ( 'id' INTEGER PRIMARY KEY, 'name' VARCHAR(255), 'description' VARCHAR(255), 'icon' VARCHAR(255), 'scores' INTEGER ); 
--- db/SQL/CREATE_USERS.sql
+++
@@ -1,2 +0,0 @@
--- SQLite
-CREATE TABLE 'users' ( 'id' INTEGER PRIMARY KEY, 'name' VARCHAR(255), 'email' VARCHAR(255), 'password' VARCHAR(255), 'photo' VARCHAR(255) ); 
